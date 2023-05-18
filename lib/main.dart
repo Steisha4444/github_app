3689e4bbe36df6eb43a0145877d02dfe80910017
Steisha4444 <shvednastya0@gmail.com>
2023-05-18 17:06:40 +0400

    complete app

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:get/get.dart';
 import 'package:github_app/core/app_theme.dart';
+import 'package:github_app/modules/github_search/screens/github_search_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    FlutterNativeSplash.remove();
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: AppTheme.buildTheme(context),
-      home: const Scaffold(body: Text('GitHub App')),
+      home: GitHubScreen(),
     );
   }
 }
